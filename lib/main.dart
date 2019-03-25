2dd8bf18a7ea0477b09a533aa9fced46a6f1c7ac
conghao <780403@nd.com>
2019-03-26 01:12:19 +0800

    联系操作符重载

--- lib/main.dart
+++ lib/main.dart
@@ -4,12 +4,37 @@ import 'package:http/http.dart' as http;
 import 'dart:convert';
 import 'dart:io';
 
+class Vector {
+  final int x;
+  final int y;
+  const Vector(this.x, this.y);
+
+  Vector operator + (Vector v) {
+    return new Vector(x + v.x, y + v.y);
+  }
+  Vector operator - (Vector v) {
+    return new Vector(x - v.x, y - v.y);
+  }
+}
+/*
+main() {
+  final v = new Vector(2, 3);
+  final w = new Vector(2, 2);
+  final r1 = v + w;
+  print('r1.x = ' + r1.x.toString()  + 'r1.y = ' + r1.y.toString());
+}
+*/
+
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    final v = new Vector(2, 3);
+    final w = new Vector(2, 2);
+    final r1 = v + w;
+    print('r1.x = ' + r1.x.toString()  + 'r1.y = ' + r1.y.toString());
     return MaterialApp(
       title: 'Welcome to Flutter',
       theme: new ThemeData(
